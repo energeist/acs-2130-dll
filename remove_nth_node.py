2e61742d43b15c393c9301abdf6f0fbff0371175
Mark Rattle <mark.rattle@gmail.com>
2023-04-20 22:34:18 -0400

    comments

---
+++ remove_nth_node.py
@@ -0,0 +1,12 @@
+from linkedlist import LinkedList
+
+def remove_nth_node(linked_list):
+    pass
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
--- reversed_linkedlist.py
+++ reversed_linkedlist.py
@@ -1,15 +1,24 @@
-from linkedlist import Node
 from linkedlist import LinkedList
 
 def reverse_ll(forward_list):
+    # assume that as a final product we only want one list and set of nodes remaining (not None)
     reversed_list = LinkedList()
     # append the head of the forward list to a new reversed linked list
     reversed_list.append(forward_list.head.data)
+    # initialize current_node pointer
     current_node = forward_list.head
-    while current_node is not None:
-        current_node = current_node.next
-        if current_node is not None:
-            reversed_list.prepend(current_node.data)
+    while current_node:
+        # set the next_node pointer
+        next_node = current_node.next
+        # set current_node reference from original list to None
+        current_node = None
+        if next_node:
+            # prepend the next_node's data to a list in a new node, if it exists
+            reversed_list.prepend(next_node.data)
+        # reset current_node pointer for next iteration
+        current_node = next_node
+    # remove reference to the original list
+    forward_list = None
     return reversed_list
 
 forward_ll = LinkedList()
