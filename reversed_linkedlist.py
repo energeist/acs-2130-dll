9138795ddbb8b8dcc5ea8d48ca50b927f8db7d36
Mark Rattle <mark.rattle@gmail.com>
2023-04-21 12:50:09 -0400

    made reversed ll better

--- reversed_linkedlist.py
+++ reversed_linkedlist.py
@@ -21,11 +21,28 @@ def reverse_ll(forward_list):
     forward_list = None
     return reversed_list
 
+def reverse_ll_in_place(ll):
+    # code to reverse linked list in place
+    # iterate over list and prepend each node back on the list
+    current_node = ll.head
+    while current_node:
+        # head node doesn't need to be manipulated
+        if current_node == ll.head:
+            current_node = current_node.next
+            ll.head.next = None
+        ll.prepend(current_node.data)
+        current_node = current_node.next
+    return ll
+        
+        
 forward_ll = LinkedList()
 forward_ll.append(1)
 forward_ll.append(2)
 forward_ll.append(3)
-reversed_ll = reverse_ll(forward_ll)
+reverse_in_place = reverse_ll_in_place(forward_ll)
+# reversed_ll = reverse_ll(forward_ll)
 print(f"Forward linked list: {forward_ll}")
-print(f"Reversed linked list: {reversed_ll}")
+# print(f"Reversed linked list: {reversed_ll}")
+print(f"Reversed linked list: {reverse_in_place}")
+
         
