234e8d89c37250a4186fc0fb9c2db81bf9807e73
Mark Rattle <mark.rattle@gmail.com>
2023-04-20 22:52:13 -0400

    remove nth last node

--- remove_nth_last_node.py
+++ remove_nth_last_node.py
@@ -1,12 +1,14 @@
 from linkedlist import LinkedList
 
-def remove_nth_node(linked_list, n):
+def remove_nth_last_node(linked_list, n):
     # initialize current_node as the head of the linked list
     current_node = linked_list.head
+    length = linked_list.length()
     # iterate over linked list while counter value is greater than 1 
     # head node is n=1 and doesn't need to be double counted
-    while n > 1:
+    while length > n:
         current_node = current_node.next
+        length -= 1
     linked_list.delete(current_node.data)
     return linked_list
 
@@ -17,8 +19,8 @@ ll.append(5)
 ll.append(7)
 ll.append(9)
 
-print(ll) # (1) -> (3) -> (5) -> (7) -> (9) -> || (3) should be removed if n = 2
-print(remove_nth_node(ll, 2)) # (1) -> (5) -> (7) -> (9) -> 
+print(ll) # (1) -> (3) -> (5) -> (7) -> (9) -> || (7) should be removed if n = 2
+print(remove_nth_last_node(ll, 2)) # (1) -> (3) -> (5) -> (9) ->
 
 # I have chosen to use the built in linkedlist methods.  Note that it would be more 
 # computationally efficient to manipulate nodes directly.
